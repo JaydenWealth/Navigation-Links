54a489a295e9765f355d94f6b26c271098f1bc3c
Awogbami Tunde <oladipupo.jayden@gmail.com>
2024-09-08 21:52:14 +0100

    Navigation First Commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Toggle hamburger menu and nav links
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
